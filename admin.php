f7d569b2671104229eab21b87fc295c283f0013b
danuRifanda <danunandika0@gmail.com>
2023-07-07 20:46:59 +0700

    benerin login

--- admin.php
+++ admin.php
@@ -17,14 +17,14 @@ if (empty($_SESSION['ussername'])){
   <body>
     <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid container">
-        <a class="navbar-brand" href="pegawai.php">Admin</a>
+        <a class="navbar-brand" href="admin.php">Admin</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="pegawai.php">Dashboard</a>
+            <a class="nav-link active" aria-current="page" href="admin.php">Dashboard</a>
             </li>
             <li class="nav-item">
             <a class="nav-link active" href="produk.php">Produk</a>
